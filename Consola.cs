9e303f80fca18877733915b948eae7a8e7893f47
Abraham Lipa Calabilla <abraham.lipa.calabilla@gmail.com>
2020-07-12 20:12:51 -0500

    Agrega la clase Consola (utilidades)

---
+++ Consola.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicios_Herencia_POO_Lipa
+{
+    public static class Consola
+    {
+        public static void PrintInGreen(string texto)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(texto);
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        public static void PrintInRed(string texto)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(texto);
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
